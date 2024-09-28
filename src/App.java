113ee6d35c01aba324b7c3acf1f8919c6e689c8e
dani <daniiidani883@gmail.com>
2024-09-28 18:26:06 +0700

    Updated hitung umur

--- src/App.java
+++ src/App.java
@@ -1,20 +1,5 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
-
-        System.out.println("===== Menghitung Umur Anda =====");
-        System.out.print("Masukkan tahun sekarang: ");
-        int tahun_sekarang = scanner.nextInt(); 
-
-        System.out.print("Masukkan tahun lahir: ");
-        int tahun_lahir = scanner.nextInt(); 
-
-        int umur = tahun_sekarang - tahun_lahir;
-
-        System.out.println("Umur saya: " + umur);
-
-        scanner.close(); 
+        System.out.println(" Hello World!!");
     }
 }
