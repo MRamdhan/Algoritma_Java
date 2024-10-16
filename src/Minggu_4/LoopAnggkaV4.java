80a5a44005a3901af6511c66ea2db6b0ebd2e1a4
dani <daniiidani883@gmail.com>
2024-10-16 19:46:56 +0700

    update

--- src/Minggu_4/LoopAnggkaV4.java
+++ src/Minggu_4/LoopAnggkaV4.java
@@ -2,17 +2,15 @@ package Minggu_4;
 
 public class LoopAnggkaV4 {
     public static void main(String[] args) {
-        for (int i = 0; i <= 3; i++) {
+        for (int i = 0; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
-            if (i > 0) {
-                for (int j = 0; j < i; j++) {
-                    System.out.print(j + " ");
-                }
-                System.out.println();
+            for (int j = 0; j < i; j++) {
+                System.out.print(j + " ");
             }
+            System.out.println();
         }
     }
 }
